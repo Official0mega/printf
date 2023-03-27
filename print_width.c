266b8a5928bc1db43a5d8f2b5bfbf2aed718f413
Official0mega <supr3m30m3ga@gmail.com>
2023-03-27 01:18:43 -0700

    Update

--- print_width.c
+++ print_width.c
@@ -4,16 +4,22 @@
  * get_width - Calculates the width for printing.
  * @format: Formatted string in which to print the arguments.
  * @i: List of arguments to be printed.
- * @list: list of arguments.
+ * @List: List of arguments.
  *
  * Return: width.
  */
+
 int get_width(const char *format, int *i, va_list list)
 {
-	int curr_i;
-	int width = 0;
+	int curr_i = *i + 1;
+	int width = -1;
 
-	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
+	if (format[curr_i] != '.')
+		return (width);
+
+	width = 0;
+
+	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
 	{
 		if (is_digit(format[curr_i]))
 		{
